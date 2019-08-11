a04ccbf2ee99a825a038604c768a5d28f2c1dfbb
Nirjhar <choudhurynirjhar@gmail.com>
2019-08-11 12:31:10 -0400

    First check-in

---
+++ Nunit.Demo/Nunit.Demo.Test/CalculatorTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Nunit.Demo.Test
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        Calculator calc;
+        [SetUp]
+        public void Setup()
+        {
+            calc = new Calculator();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            calc.Dispose();
+        }
+
+        [Test, Order(2)]
+        public void Test_Addition_With_Valid_Intigers()
+        {
+            var result = calc.Addition(3, 5);
+            Assert.AreEqual(8, result);
+        }
+
+        [Test, Order(1)]
+        public void Test_Substraction_Argument_Exception() {
+            Assert.Catch<SystemException>(() => calc.Substraction(4, 5));
+            Assert.Throws<ArgumentException>(() => calc.Substraction(4, 5));
+        }
+
+        [TestCase(1,2,3), Order(3)]
+        [TestCase(2, 3, 5)]
+        [TestCase(3, 4, 7)]
+        public void Test_Addition_Multiple(int first, int second, int expectedresult)
+        {
+            var calculated = calc.Addition(first, second);
+            Assert.AreEqual(expectedresult, calculated);
+        }
+
+        [Ignore("Ignore test")]
+        public void Test_To_Ignore()
+        {
+        }
+            
+    }
+}
---
+++ Nunit.Demo/Nunit.Demo/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Nunit.Demo
+{
+    public class Calculator : IDisposable
+    {
+        public int Addition(int first, int second)
+        {
+            return first + second;
+        }
+
+        public void Dispose()
+        {
+            // todo
+        }
+
+        public int Substraction(int first, int second)
+        {
+            if (first < second)
+                throw new ArgumentException($"First number {first} is less than second number {second}");
+
+            return first - second;
+        }
+    }
+}
